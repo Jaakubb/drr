1293bdc1c3f8c9b4edd1dc9b830d08ed5674c757
Jaakubb <jakub.tabaka@onet.pl>
2022-06-25 18:22:01 +0200

    f

---
+++ src/main/java/com/example/demo/main.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class main {
+    public static void main () throws IOException{
+        String url = "http://localhost:8080/logowanie";
+        URL obj = new URL(url);
+        HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("User-Agent", "test");
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(connection.getInputStream()));
+        String inputline;
+        StringBuffer odpowiedz = new StringBuffer();
+        while()
+
+    }
+}
